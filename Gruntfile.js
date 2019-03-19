e54f9e4fba12042b7f07a2790fcb5dc6e50d6a9e
IlyaVento <ilya.popov.sbmt@gmail.com>
2019-03-19 21:44:11 +0300

    Testem and Jasmine added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,41 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         jshint: {
-            all: ['ventoJScore/src/**/*.js'],
+            all: ['ventoJScore/**/**/*.js'],
             options: {
                 globals: {
                     _: false,
-                    $: false
+                    $: false,
+                    jasmine: false,
+                    describe: false,
+                    it: false,
+                    expect: false,
+                    beforeEach: false,
+                    afterEach: false,
+                    sinon: false
                 },
                 browser: true,
                 devel: true
+            }
+        },
+        testem: {
+            unit: {
+                options: {
+                    framework: 'jasmin2',
+                    launch_in_dev: ['PhantomJS'],
+                    before_tests: 'grunt jshint',
+                    serve_files: [
+                        'node_modules/sinon/pkg/sinon.js',
+                        'ventoJScore/**/**/*.js'
+                    ],
+                    watch_files: [
+                        'ventoJScore/**/**/*.js'
+                    ]
+                }
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-testem');
 };
